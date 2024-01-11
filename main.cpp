ea60e46643efc009c79068898e82f7b453c048c3
Chaitanya Sharma <62926067+chaitanyas22@users.noreply.github.com>
2024-01-11 17:44:51 +0530

    functions staffid, issuebook and few are working but validations are remianing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <sstream>
 
@@ -12,14 +13,11 @@ public:
     string authorLastName;
     string bookType;
 
-
     Book(int bkId, const string& bkName, const string& arFirstName, const string& arLastName, const string& bkType)
         : bookId(bkId), bookName(bkName), authorFirstName(arFirstName), authorLastName(arLastName), bookType(bkType) {}
 
-        // this function will call all the book id when called in the function
-
-    void displayBook() const {
-        cout << "Book Id: " << bookId << "\nBook Name: " << bookName
+    void displayInfo() const {
+        cout << "Book ID: " << bookId << "\nBook Name: " << bookName
              << "\nAuthor First Name: " << authorFirstName << "\nAuthor Last Name: " << authorLastName
              << "\nBook Type: " << bookType << "\n\n";
     }
@@ -31,7 +29,21 @@ public:
     string name;
     string address;
     string email;
+
+    Member(int memID, const string& memName, const string& memAddress, const string& memEmail)
+        : memberID(memID), name(memName), address(memAddress), email(memEmail) {}
 };
+
+class BooksBorrowed {
+public:
+    int borrower;
+    int bbookid;
+
+    BooksBorrowed(int bborrower, int bbkid)
+        : borrower(bborrower), bbookid(bbkid) {}
+};
+
+
 
 class Librarian {
 public:
@@ -40,12 +52,76 @@ public:
     string name;
     string address;
     string email;
-};
+    vector<Member> members;
+    vector<BooksBorrowed> borrowedBooks;  
 
+    void setStaffId() {
+        cout << "Set Librarian staff ID: ";
+        cin >> staffID;
+    }
+
+    void getStaffId() const {
+        cout << "Staff ID is: " << staffID << "\n";
+    }
+
+    void getSalary() const {
+        cout << "Staff salary is: $" << salary << "\n";
+    }
+
+    void setSalary() {
+        cout << "Enter the salary of the librarian: ";
+        cin >> salary;
+    }
+
+    void issueBook() {
+        int bborrower, bbkid;
+
+        cout << "Enter the Book Id to be issued: ";
+        cin >> bbkid;
+        cout << "Enter the Member id to whom the book is issued: ";
+        cin >> bborrower;
+
+        borrowedBooks.emplace_back(bborrower, bbkid);
+        cout << "Book issued to the member successfully.\n";
+    }
+
+    void displayBorrowedBooks() const {
+        cout << "List of books issued to members:\n";
+        for (const auto& borrowedBook : borrowedBooks) {
+            cout << "Member ID: " << borrowedBook.borrower << "\nBook ID: " << borrowedBook.bbookid << "\n\n";
+        }
+    }
+
+    void addMember() {
+        int memID;
+        string memName, memAddress, memEmail;
+
+        cout << "Enter Member ID: ";
+        cin >> memID;
+        cout << "Enter Member Name: ";
+        cin.ignore(); 
+        getline(cin, memName);
+        cout << "Enter Member Address: ";
+        getline(cin, memAddress);
+        cout << "Enter Member Email: ";
+        getline(cin, memEmail);
+
+        members.emplace_back(memID, memName, memAddress, memEmail);
+        cout << "Member added successfully.\n";
+    }
+
+    void displayMembers() const {
+        cout << "List of Members:\n";
+        for (const auto& member : members) {
+            cout << "Member ID: " << member.memberID << "\nName: " << member.name
+                 << "\nAddress: " << member.address << "\nEmail: " << member.email << "\n\n";
+        }
+    }
+};
 
 void processCSVData(const vector<Book>& books) {
     for (const auto& book : books) {
-        book.displayBook();
+        book.displayInfo();
     }
 }
 
@@ -73,9 +149,26 @@ int main() {
 
     file.close();
 
-    books.displayBooks();
+    Librarian librarian;
+    // librarian.availableBooks = books;  // Removed this line as availableBooks is not used in the code
 
-     return 0;
+    int firstInput;
+
+    cout << "Welcome to the Library management system \n";
+
+    cout << "TO ISSUE BOOK = 1  ||  TO RETURN BOOK = 2 || TO ADD MEMBER = 3 \n";
+    cin >> firstInput;
+
+    if (firstInput == 1) {
+        librarian.issueBook();
+    } else if (firstInput == 2) {
+       
+    } else if (firstInput == 3) {
+        librarian.addMember();
+    }
+
+    librarian.displayMembers();
+    librarian.displayBorrowedBooks();
+
+    return 0;
 }
-
-
